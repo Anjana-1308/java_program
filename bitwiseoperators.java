0c5639af9a700cf4a12b6ff2efa09ee26a14b53b
Anjana-1308 <anjanan1308@gmail.com>
2025-03-01 22:30:53 -0800

    upload

---
+++ bitwiseoperators.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class bitwiseoperators 
+
+{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("enter the a value:");
+        int a = obj.nextInt();
+        System.out.print("enter the b value:");
+        int b = obj.nextInt();
+        System.out.print("enter the c value:");
+        int c = obj.nextInt();
+        System.out.println("AND:"+(a&b));
+        System.out.println("OR:"+(a|b));
+        System.out.println("XOR:"+(a^b));
+        System.out.println("left shift:"+(c<<2));
+        System.out.println("right shift:"+(c>>2));
+        
+    }
+}
