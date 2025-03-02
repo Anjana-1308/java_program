fef180acae45101cb011da93bcc47127b7c61002
Anjana-1308 <anjanan1308@gmail.com>
2025-03-01 22:54:05 -0800

    upload

---
+++ assignmentoperators.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class assignmentoperators 
+{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("enter the a value:");
+        int a = obj.nextInt();
+        System.out.print("enter the b value:");
+        int b = obj.nextInt();
+        a+=b;
+        System.out.println(a);
+        a-=b;
+        System.out.println(a);
+        a*=b;
+        System.out.println(a);
+        a/=b;
+        System.out.println(a);
+        a%=b;
+        System.out.println(a);
+        
+    }
+}
