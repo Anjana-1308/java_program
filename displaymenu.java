c0d0112bdeb7464a6f34043a9769f5064c0bde4d
Anjana-1308 <anjanan1308@gmail.com>
2025-03-06 15:51:33 -0800

    upload

---
+++ displaymenu.java
@@ -0,0 +1,39 @@
+
+
+import java.util.Scanner;
+
+public class displaymenu 
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Option 1");
+            System.out.println("2. Option 2");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Executing Option 1...");
+                    // Add code for option 1
+                    break;
+                case 2:
+                    System.out.println("Executing Option 2...");
+                    // Add code for option 2
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 3);
+
+        scanner.close();
+    }
+}
