774591b52148d2064f040ba6c4c1ccad4b4e2e9c
Anjana-1308 <anjanan1308@gmail.com>
2025-03-06 19:23:42 -0800

    upload

---
+++ breakprgm.java
@@ -0,0 +1,16 @@
+
+
+
+
+
+public class breakprgm 
+{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i == 13) {
+                break; // Exit the loop when i is 13
+            }
+            System.out.println(i);
+        }
+    }
+}
